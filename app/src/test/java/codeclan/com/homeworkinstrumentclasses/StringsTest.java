b0fd8c3e102ca4bb5b6d9656a76ba9acc10d7e79
Miguel <miguelmoutela@hotmail.com>
2017-11-04 11:46:41 +0000

    Strings class => all tests passing

--- app/src/test/java/codeclan/com/homeworkinstrumentclasses/StringsTest.java
+++ app/src/test/java/codeclan/com/homeworkinstrumentclasses/StringsTest.java
@@ -1,8 +1,48 @@
 package codeclan.com.homeworkinstrumentclasses;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import codeclan.com.homeworkinstrumentclasses.Instruments.Strings;
+
+import static codeclan.com.homeworkinstrumentclasses.Instruments.InstrumentType.ACOUSTIC;
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by user on 03/11/2017.
  */
 
 public class StringsTest {
+    Strings guitar;
+
+    @Before
+    public void before() {
+        guitar = new Strings(ACOUSTIC, 20,25,7);
+    }
+    @Test
+    public void canGetMarkUp() {
+        assertEquals(0.25, guitar.markUp(),0.01);
+    }
+    @Test
+    public void canGetSoundItMakes() {
+        assertEquals("Squeak Squeak", guitar.getSoundItMakes());
+    }
+    @Test
+    public void canGetInstrumentType() {
+        assertEquals(ACOUSTIC, guitar.getInstrumentType());
+    }
+    @Test
+    public void canGetCostPrice() {
+        assertEquals(20, guitar.getCostPrice(),0.01);
+    }
+    @Test
+    public void canGetSellPrice() {
+        assertEquals(25, guitar.getSellPrice(),0.01);
+    }
+    @Test
+    public void canGetNumberOfStrings() {
+        assertEquals(7, guitar.getNumberOfStrings());
+    }
+
+
 }
