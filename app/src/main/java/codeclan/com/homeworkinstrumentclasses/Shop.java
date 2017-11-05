ec3b0ef7f143608b63f4f022528df4b1d61a1897
Miguel <miguelmoutela@hotmail.com>
2017-11-05 12:55:56 +0000

    ShopTest 4 tests passing

--- app/src/main/java/codeclan/com/homeworkinstrumentclasses/Shop.java
+++ app/src/main/java/codeclan/com/homeworkinstrumentclasses/Shop.java
@@ -1,11 +1,29 @@
 package codeclan.com.homeworkinstrumentclasses;
 
 import java.util.ArrayList;
+import java.util.ArrayList.*;
+
+
+import codeclan.com.homeworkinstrumentclasses.Instruments.Strings;
 
 /**
  * Created by user on 04/11/2017.
  */
 
 public class Shop {
-   private ArrayList<Sellable>stock;
+     String name;
+     ArrayList<Sellable>items;
+
+    public Shop(String name) {
+        this.name = name;
+        this.items = new ArrayList<Sellable>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return items.size();
+    }
 }
--- app/src/test/java/codeclan/com/homeworkinstrumentclasses/ShopTest.java
+++ app/src/test/java/codeclan/com/homeworkinstrumentclasses/ShopTest.java
@@ -1,8 +1,56 @@
 package codeclan.com.homeworkinstrumentclasses;
+import java.util.ArrayList;
+import java.util.ArrayList.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import codeclan.com.homeworkinstrumentclasses.Accesssories.Cables;
+import codeclan.com.homeworkinstrumentclasses.Instruments.Drums;
+
+import codeclan.com.homeworkinstrumentclasses.Instruments.Strings;
+import static codeclan.com.homeworkinstrumentclasses.Instruments.InstrumentType.ELECTRIC;
+import static junit.framework.Assert.assertEquals;
+
 
 /**
  * Created by user on 04/11/2017.
  */
 
 public class ShopTest {
+
+    Shop musicShop;
+    Strings guitar;
+    Drums drums;
+    Cables cables;
+
+
+    @Before
+    public void before() {
+        musicShop = new Shop("thatShop");
+        guitar = new Strings(ELECTRIC, 100,145,6);
+        drums = new Drums(ELECTRIC,400,500,3,5,7);
+        cables = new Cables(45,65,20);
+    }
+    @Test
+    public void shopHasName() {
+        assertEquals("thatShop", musicShop.getName());
+    }
+    @Test
+    public void shopIsEmpty() {
+        assertEquals(0, musicShop.getSize());
+    }
+    @Test
+    public void canAddItemsToShop() {
+        musicShop.items.add(guitar);
+        assertEquals(1, musicShop.getSize());
+    }
+    @Test
+    public void canRemoveItemsFromShop() {
+        musicShop.items.add(guitar);
+        musicShop.items.remove(guitar);
+        assertEquals(0, musicShop.getSize());
+    }
+    
+
 }
